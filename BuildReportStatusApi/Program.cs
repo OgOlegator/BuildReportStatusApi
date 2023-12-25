ed59fa8b0d304a83f6a0a61e0dc1f0b72e951ecc
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-12-25 23:17:58 +0300

    Раскомментировано добавления контекста в DI для миграций в БД

--- BuildReportStatusApi/Program.cs
+++ BuildReportStatusApi/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddDbContext<AppDbContext>(options
-//    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ITaskHeaderRepository, TaskHeaderRepository>();
 
